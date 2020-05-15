0e87641bf6110db71b4cbe4ca5d376a70f9f893c
kwasniew <kwasniewski.mateusz@gmail.com>
2020-05-15 15:58:43 +0200

    Modelling state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import htm from "./web_modules/htm.js";
 const html = htm.bind(h);
 
 const state = {
+  currentPostText: "type your text",
   posts: [
     {
       username: "js_developers",
@@ -12,6 +13,11 @@ const state = {
     { username: "js_developers", body: "Modern JS frameworks are too heavy" },
     { username: "jorgebucaran", body: "There, I fixed it for you!" },
   ],
+};
+
+const AddPost = (state) => {
+  const newPost = { username: "fixed user", body: "fixed text" };
+  return { ...state, posts: [newPost, ...state.posts] };
 };
 
 const listItem = (post) => html`
@@ -24,6 +30,8 @@ const listItem = (post) => html`
 const view = (state) => html`
   <div>
     <h1>Recent Posts</h1>
+    <input type="text" value=${state.currentPostText} autofocus />
+    <button onclick=${AddPost}>Add Post</button>
     <ul>
       ${state.posts.map(listItem)}
     </ul>
