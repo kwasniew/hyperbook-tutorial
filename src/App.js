2864c68c65520210272516bf7c224aa4b2315633
kwasniew <kwasniewski.mateusz@gmail.com>
2020-05-15 13:23:29 +0200

    Analyzing view rendering options

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import {h, app} from "https://unpkg.com/hyperapp?module";
+import htm from 'https://unpkg.com/htm?module';
+
+const html = htm.bind(h);
 
 const state = {text: "Welcome to Hyperapp!"};
 
 app({
     init: state,
-    view: state => h("h1", {id: "my-header"}, state.text),
+    view: state => html`<h1 id="my-header"><span>${state.text}</span></h1>`,
     node: document.getElementById("app")
 });
