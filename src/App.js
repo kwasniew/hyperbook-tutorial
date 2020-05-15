95768584504224e5569fb281255f066d322e5737
kwasniew <kwasniewski.mateusz@gmail.com>
2020-05-15 14:37:27 +0200

    Formatting code with prettier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import {h, app} from "./web_modules/hyperapp.js";
+import { h, app } from "./web_modules/hyperapp.js";
 import htm from "./web_modules/htm.js";
 
 const html = htm.bind(h);
 
-const state = {text: "Welcome to Hyperapp!"};
+const state = { text: "Welcome to Hyperapp!" };
 
 app({
-    init: state,
-    view: state => html`<h1 id="my-header"><span>${state.text}</span></h1>`,
-    node: document.getElementById("app")
+  init: state,
+  view: (state) => html`
+    <div>
+      <h1>Recent Posts</h1>
+      <ul>
+        <li>
+          <strong>@js_developers</strong>
+          <span>Modern JS frameworks are too complicated</span>
+        </li>
+        <li>
+          <strong>@jorgebucaran</strong>
+          <span>There, I fixed it for you!</span>
+        </li>
+      </ul>
+    </div>
+  `,
+  node: document.getElementById("app"),
 });
