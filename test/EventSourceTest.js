f7142e94b18a04b9d5b6a59191d103937a6fe34f
kwasniew <kwasniewski.mateusz@gmail.com>
2020-05-17 17:51:49 +0200

    Testing effects and subscriptions

---
+++ test/EventSourceTest.js
@@ -0,0 +1,58 @@
+import assert from "assert";
+import { EventSourceListen } from "../src/lib/EventSource.js";
+
+const givenEventSource = (serverUrl) => {
+  const listeners = {};
+  let isClosed = false;
+  const es = {
+    addEventListener(name, listener) {
+      listeners[name] = listener;
+    },
+    removeEventListener(name, listener) {
+      delete listeners[name];
+    },
+    close() {
+      isClosed = true;
+    },
+  };
+  const emit = (event) => {
+    Object.values(listeners).forEach((listener) => {
+      listener(event);
+    });
+  };
+
+  function EventSource(url) {
+    if (url === serverUrl) return es;
+  }
+  global.EventSource = EventSource;
+
+  return { emit, isClosed: () => isClosed };
+};
+
+const runFx = ([effect, data]) => {
+  const dispatch = (action, event) => (dispatch.invokedWith = [action, event]);
+  const unsubscribe = effect(dispatch, data);
+  return { dispatch, unsubscribe };
+};
+
+describe("Event source subscription", () => {
+  const defaultEventSource = global.EventSource;
+  afterEach(() => {
+    global.EventSource = defaultEventSource;
+  });
+
+  it("dispatches events", () => {
+    const { emit } = givenEventSource("http://example.com");
+    const { dispatch } = runFx(
+      EventSourceListen({ url: "http://example.com", action: "action" })
+    );
+
+    emit({ data: "event data" });
+
+    assert.deepStrictEqual(dispatch.invokedWith, [
+      "action",
+      { data: "event data" },
+    ]);
+  });
+
+});
