35f546f30b7bbc48126facea59463dbbcd0e4931
kwasniew <kwasniewski.mateusz@gmail.com>
2020-05-18 13:41:00 +0200

    Hydrating server side code on the client side

--- Server.js
+++ Server.js
@@ -1,17 +1,41 @@
 import render from "hyperapp-render";
 import express from "express";
 import { state, view, SetPosts } from "./src/Posts.js";
+import {layout} from "./src/Layout.js";
 import axios from "axios";
 
 const app = express();
 
+const htmlTemplate = (content) => /* HTML */ `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>HyperPosts</title>
+      <link rel="stylesheet" href="https://andybrewer.github.io/mvp/mvp.css" />
+      <script type="module" src="Start.js"></script>
+    </head>
+    <body>
+      <main>
+        <div id="app">${content}</div>
+      </main>
+    </body>
+  </html>
+`;
+
+
 app.get("/", async (req, res) => {
-  const response = await axios.get("https://hyperapp-api.herokuapp.com/api/post");
+  const response = await axios.get(
+    "https://hyperapp-api.herokuapp.com/api/post"
+  );
   const posts = response.data;
   const stateWithPosts = SetPosts(state, posts);
-  const html = render.renderToString(view(stateWithPosts));
-  res.send(html);
+  const content = render.renderToString(layout(view)(stateWithPosts));
+  res.send(htmlTemplate(content));
 });
+app.use(express.static("src"));
+
 app.listen(3000, () => {
   console.log("Listening on 3000");
 });
